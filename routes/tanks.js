fcc01793d9f42aa17456531bc352b2e8184a941c
Teffisk <brad.mcknight91@gmail.com>
2019-01-06 17:49:45 -0800

    modules on show page

--- routes/tanks.js
+++ routes/tanks.js
@@ -15,28 +15,27 @@ router.get('/list', function(req, res) {
 });
 
 router.get('/show/:id', function(req, res) {
-	db.tank.findByPk(req.params.id).
-	then(function(tank) {
-		const urlTanks = `https://api-xbox-console.worldoftanks.com/wotx/encyclopedia/vehiclepackages/?application_id=1713b4e1f4383b7a11a4f24c86f8cefa&tank_id=${tank.tank_id}`
-		request(urlTanks, function(error, response, body) {
-			modules = JSON.parse(body);
-		})
-	})
-	.then(function(package) {
-		const urlPackages = `https://api-xbox-console.worldoftanks.com/wotx/encyclopedia/vehiclepackages/?application_id=1713b4e1f4383b7a11a4f24c86f8cefa&tank_id=${tank.tank_id}`
-		request(urlPackages, function(error, response, body){
-			var packageData = JSON.parse(body).data.packagetree
-			for (var package in packageData) {
-				packages.push(packageData[package])
+	var packageNums = [];
+	var tank = db.tank.findByPk(req.params.id).then(function(tank){
+		const urlPackages = `https://api-xbox-console.worldoftanks.com/wotx/encyclopedia/vehiclepackages/?application_id=1713b4e1f4383b7a11a4f24c86f8cefa&tank_id=${tank.tank_id}`;
+		request(urlPackages, function(error, response, body) {
+			var packageData = JSON.parse(body).data
+			for (var packages in packageData) {
+				packageNums.push(packageData[packages])
 			}
+		res.render('tanks/show', { packageNums: packageNums, tank: tank })
 		})
-		res.render('tanks/show', { tank: tank, package: package });
 	})
-	.catch(function(err){
-		console.log("Error:", err)
-		res.send("Error:", err)
-	})
-	
 });
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
